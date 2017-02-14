5fb4e6500a9ce9e5a2bb10006f99e44b23734089
Michael Penberthy <mike@type2designs.com>
2017-02-14 15:53:06 -0500

    update

--- cloud/main.js
+++ cloud/main.js
@@ -11,7 +11,23 @@ var twilioPhoneNumber = '+13146268180';
 var twilio = require('twilio');
 var client = new twilio.RestClient(twilioAccountSid, twilioAuthToken);
 
+var user = new Parse.User();
+user.set("username", "my name");
+user.set("password", "my pass");
+user.set("email", "email@example.com");
 
+// other fields can be set just like with Parse.Object
+user.set("phone", "415-392-0202");
+
+user.signUp(null, {
+  success: function(user) {
+    // Hooray! Let them use the app now.
+  },
+  error: function(user, error) {
+    // Show the error message somewhere and let the user try again.
+    alert("Error: " + error.code + " " + error.message);
+  }
+});
 
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
