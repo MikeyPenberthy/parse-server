9572a83ff238e2e68d707c9c3782d04d88ff60c7
Michael Penberthy <mike@type2designs.com>
2017-02-14 16:27:01 -0500

    update

--- cloud/main.js
+++ cloud/main.js
@@ -11,11 +11,23 @@ var twilioPhoneNumber = '+13146268180';
 var twilio = require('twilio');
 var client = new twilio.RestClient(twilioAccountSid, twilioAuthToken);
 
-ParseObject gameScore = new ParseObject("GameScore");
-gameScore.put("score", 1337);
-gameScore.put("playerName", "Sean Plott");
-gameScore.put("cheatMode", false);
-gameScore.saveInBackground();
+var user = new Parse.User();
+user.set("username", "my name");
+user.set("password", "my pass");
+user.set("email", "email@example.com");
+
+// other fields can be set just like with Parse.Object
+user.set("phone", "415-392-0202");
+
+user.signUp("name", "password", {} {
+  success: function(user) {
+    // Hooray! Let them use the app now.
+  },
+  error: function(user, error) {
+    // Show the error message somewhere and let the user try again.
+    alert("Error: " + error.code + " " + error.message);
+  }
+});
 
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
