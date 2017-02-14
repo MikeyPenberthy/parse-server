44142c240f691654fcf3403c3cd91c0de5f366c9
Michael Penberthy <mike@type2designs.com>
2017-02-14 15:18:37 -0500

    update

--- cloud/main.js
+++ cloud/main.js
@@ -46,46 +46,9 @@ Parse.Cloud.define("VerifyAccount", function(request, response) {
 					body: 'Your login code for Watch Your BAC is '+ token
 				}, function(err, responseData) {});
 	
-	
+
 	response.success(prefix + phoneNumber);
-	// if (result) {
-	// 		console.log("Verifying existing user");
-	// 		result.setPassword(pass);
-	// 		result.save().then(function() {
-				
-	// 			client.messages.create({
-	// 				to: prefix + phoneNumber,
-	// 				from: twilioPhoneNumber,
-	// 				body: 'Your login code for Watch Your BAC is '+ token
-	// 			}, function(err, responseData) {});
-
-	// 		}).then(function() {
-	// 			response.success();
-	// 		}, function(err) {
-	// 			response.error(err);
-	// 		});
-	// 	}
-	// 	else {
-	// 		var user = (request.user && !request.user.get("verified")) ? request.user
-	// 			: new Parse.User();
-
-	// 		user.setUsername(phoneNumber);
-	// 		user.setPassword(pass);
-	// 		user.save().then(function(a) {
-	// 			client.messages.create({
-	// 				to: prefix + phoneNumber,
-	// 				from: twilioPhoneNumber,
-	// 				body: 'Your login code for Watch Your BAC is '+ token
-	// 			}, function(err, responseData) {});
-	// 		}).then(function() {
-	// 			response.success();
-	// 		}, function(err) {
-	// 			response.error(err);
-	// 		});
-	// 	}
-	// }, function (err) {
-	// 	response.error(err);
-	// });
+});
 });
 
 
