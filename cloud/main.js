2fdd1c276b320ae6f4e6efc1336a93e6eb9a685d
Michael Penberthy <mike@type2designs.com>
2017-02-14 12:05:05 -0500

    phone test 5

--- cloud/main.js
+++ cloud/main.js
@@ -19,55 +19,54 @@ Parse.Cloud.define('hello', function(req, res) {
 Parse.Cloud.define("VerifyAccount", function(request, response) {
 	var phoneNumber = request.params.phone;
 	var countryCode = request.params.countryCode;
-	return sendCodeSms(countryCode, phoneNumber, token);
-	// phoneNumber = phoneNumber.replace(/\D/g, '');
+	phoneNumber = phoneNumber.replace(/\D/g, '');
 
-	// // Validate the phone number - US only
-	// if (!countryCode) {
-	// 	return response.error("Missing country code");
-	// }
-	// if (!phoneNumber || (phoneNumber.length != 10 && phoneNumber.length != 11)) {
-	// 	return response.error('Invalid Parameters');
-	// }
+	// Validate the phone number - US only
+	if (!countryCode) {
+		return response.error("Missing country code");
+	}
+	if (!phoneNumber || (phoneNumber.length != 10 && phoneNumber.length != 11)) {
+		return response.error('Invalid Parameters');
+	}
 
-	// Parse.Cloud.useMasterKey();
-	// var query = new Parse.Query(Parse.User);
-	// query.equalTo('username', phoneNumber + "");
-	// query.first().then(function(result) {
-	// 	var min = 100; var max = 999;
-	// 	var num1 = Math.floor(Math.random() * (max - min + 1)) + min;
-	// 	var num2 = Math.floor(Math.random() * (max - min + 1)) + min;
-	// 	var token = num1 + " " + num2;
-	// 	var pass = token.replace(/\D/g, '');
+	Parse.Cloud.useMasterKey();
+	var query = new Parse.Query(Parse.User);
+	query.equalTo('username', phoneNumber + "");
+	query.first().then(function(result) {
+		var min = 100; var max = 999;
+		var num1 = Math.floor(Math.random() * (max - min + 1)) + min;
+		var num2 = Math.floor(Math.random() * (max - min + 1)) + min;
+		var token = num1 + " " + num2;
+		var pass = token.replace(/\D/g, '');
 
-	// 	if (result) {
-	// 		console.log("Verifying existing user");
-	// 		result.setPassword(pass);
-	// 		result.save().then(function() {
-	// 			return sendCodeSms(countryCode, phoneNumber, token);
-	// 		}).then(function() {
-	// 			response.success();
-	// 		}, function(err) {
-	// 			response.error(err);
-	// 		});
-	// 	}
-	// 	else {
-	// 		var user = (request.user && !request.user.get("verified")) ? request.user
-	// 			: new Parse.User();
+		if (result) {
+			console.log("Verifying existing user");
+			result.setPassword(pass);
+			result.save().then(function() {
+				return sendCodeSms(countryCode, phoneNumber, token);
+			}).then(function() {
+				response.success();
+			}, function(err) {
+				response.error(err);
+			});
+		}
+		else {
+			var user = (request.user && !request.user.get("verified")) ? request.user
+				: new Parse.User();
 
-	// 		user.setUsername(phoneNumber);
-	// 		user.setPassword(pass);
-	// 		user.save().then(function(a) {
-	// 			return sendCodeSms(countryCode, phoneNumber, token);
-	// 		}).then(function() {
-	// 			response.success();
-	// 		}, function(err) {
-	// 			response.error(err);
-	// 		});
-	// 	}
-	// }, function (err) {
-	// 	response.error(err);
-	// });
+			user.setUsername(phoneNumber);
+			user.setPassword(pass);
+			user.save().then(function(a) {
+				return sendCodeSms(countryCode, phoneNumber, token);
+			}).then(function() {
+				response.success();
+			}, function(err) {
+				response.error(err);
+			});
+		}
+	}, function (err) {
+		response.error(err);
+	});
 });
 
 Parse.Cloud.define("login", function(request, response) {
@@ -115,8 +114,8 @@ function sendCodeSms(countryCode, phoneNumber, token) {
 	var promise = new Parse.Promise();
 	// .replace(/\D/g, '')
 	twilio.sendSms({
-		to: "+1" + phoneNumber,
-		from: twilioPhoneNumber,
+		to: "+1" + phoneNumber.replace(/\D/g, ''),
+		from: twilioPhoneNumber.replace(/\D/g, ''),
 		body: 'Your login code for Watch Your BAC is ' + token
 	}, function(err, responseData) {
 		if (err) {
